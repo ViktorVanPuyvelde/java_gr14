1d069fd4109163bf012dbf7cd89ad78077f7cd4c
Viktor Van Puyvelde <viktor.vanpuyvelde2@student.hogent.be>
2022-04-23 18:15:12 +0200

    making categorie class an entity

--- src/domein/Categorie.java
+++ src/domein/Categorie.java
@@ -1,10 +1,26 @@
 package domein;
 
+import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 
-public class Categorie implements CRUD {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "category")
+public class Categorie implements CRUD, Serializable
+{
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "category_id")
 	private String id;
+	@Column(name = "category_name")
 	private String name;
 	private String iconName;
 	private List<String> roles;
@@ -16,33 +32,63 @@ public class Categorie implements CRUD {
 	 * @param iconName
 	 * @param roles
 	 */
-	public Categorie(String name, String iconName, List<String> roles) {
+	public Categorie(String name, String iconName, List<String> roles)
+	{
 		// TODO - implement Categorie.Categorie
 		throw new UnsupportedOperationException();
 	}
 
-	@Override
-	public void read() {
-		// TODO Auto-generated method stub
-		
+	protected Categorie()
+	{
+
 	}
 
 	@Override
-	public void update() {
+	public void read()
+	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void create() {
+	public void update()
+	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void delete() {
+	public void create()
+	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	@Override
+	public void delete()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return Objects.hash(iconName, id, name, roles, sdgs);
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categorie other = (Categorie) obj;
+		return Objects.equals(iconName, other.iconName) && Objects.equals(id, other.id)
+				&& Objects.equals(name, other.name) && Objects.equals(roles, other.roles)
+				&& Objects.equals(sdgs, other.sdgs);
+	}
 }
