107491fd47726daa0f62efb855756ac2e4a6412c
Niels Van Hove <niels.vanhove@student.hogent.be>
2022-04-25 11:18:03 +0200

    aanmelden domein

--- src/ui/ConsoleLogin.java
+++ src/ui/ConsoleLogin.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
+import com.auth0.exception.APIException;
+import com.auth0.exception.Auth0Exception;
+
 import domein.DomeinController;
 import domein.User;
 import util.JPAUtil;
@@ -25,34 +28,34 @@ public class ConsoleLogin
 		System.out.println("Paswoord:");
 		String paswoord = sc.next();
 
-//		try {
-//		dc.meldAan(email, paswoord);
-//	} catch (APIException e) {
-//		e.printStackTrace();
-//	} catch (Auth0Exception e) {
-//		e.printStackTrace();
-//	}
-//	
-//	dc.meldAf();
+		try {
+			dc.meldAan(email, paswoord);
+		} catch (APIException e) {
+			e.printStackTrace();
+		} catch (Auth0Exception e) {
+			e.printStackTrace();
+		}
+		
+		dc.meldAf();
 
-		User u = new User("161684319461fezs1", "test@test.be"/* new JSONArray() */);
-
-		List<User> lijst;
-		EntityManager en = JPAUtil.getEntityManagerFactory().createEntityManager();
-
-		en.getTransaction().begin();
-
-		TypedQuery<User> q = en.createNamedQuery("User.findAll", User.class);
-		lijst = q.getResultList();
-
-		System.out.println(lijst);
-		// en.persist(u);
-
-		en.getTransaction().commit();
-
-		en.close();
-
-		JPAUtil.getEntityManagerFactory().close();
+//		User u = new User("161684319461fezs1", "test@test.be"/* new JSONArray() */);
+//
+//		List<User> lijst;
+//		EntityManager en = JPAUtil.getEntityManagerFactory().createEntityManager();
+//
+//		en.getTransaction().begin();
+//
+//		TypedQuery<User> q = en.createNamedQuery("User.findAll", User.class);
+//		lijst = q.getResultList();
+//
+//		System.out.println(lijst);
+//		// en.persist(u);
+//
+//		en.getTransaction().commit();
+//
+//		en.close();
+//
+//		JPAUtil.getEntityManagerFactory().close();
 	}
 
 }
