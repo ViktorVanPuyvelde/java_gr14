a488bfe473ffe86aad59b8b51ff9131e4195880e
DYLAN BRACKE <DYLAN.BRACKE@student.hogent.be>
2022-04-27 13:34:57 +0200

    unused imports

--- src/gui/HomepagePaneelController.java
+++ src/gui/HomepagePaneelController.java
@@ -1,22 +1,18 @@
 package gui;
 
 import java.io.IOException;
-import java.util.ArrayList;
+
 
 import domein.DomeinController;
-import domein.Mvo;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TextField;
+
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
+
 import javafx.stage.Stage;
 
 public class HomepagePaneelController extends AnchorPane{
