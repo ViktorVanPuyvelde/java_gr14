0b7f3121b4828e140c20bbd0b180b233849c1437
DYLAN BRACKE <DYLAN.BRACKE@student.hogent.be>
2022-05-04 18:58:10 +0200

    aanmeldpaneel toegevoegd

--- src/gui/AanmeldPaneelController.java
+++ src/gui/AanmeldPaneelController.java
@@ -1,12 +1,75 @@
 package gui;
 
-import domein.DomeinController;
-import javafx.scene.layout.GridPane;
+import javafx.event.ActionEvent;
+import java.io.IOException;
 
-public class AanmeldPaneelController extends GridPane{
+import com.auth0.exception.APIException;
+import com.auth0.exception.Auth0Exception;
+
+import domein.DomeinController;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class AanmeldPaneelController extends AnchorPane{
+	
+	@FXML
+	private TextField gebruikersnaam;
+	
+	@FXML
+	private PasswordField wachtwoord;
+	
+	@FXML
+	private Label wrongLogIn;
+	
+	@FXML 
+	private Button inlogButton;
+	
+	private DomeinController dc;
 
 	public AanmeldPaneelController(DomeinController controller) {
-		// TODO Auto-generated constructor stub
+		this.dc=controller;
+		buildGui();
+		initialize();
+	}
+		
+	private void buildGui() {
+		try{
+						
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("AanmeldPaneel.fxml"));
+			loader.setController(this);
+			loader.setRoot(this);
+			loader.load();			
+			} 
+		catch(IOException ex)
+		{
+		throw new RuntimeException(ex);
+		}
+	}
+	
+	private void initialize() {
+		gebruikersnaam.setText("admin@test.be");
+		wachtwoord.setText("Azertyuiop.123");
+	}
+	
+	@FXML
+	public void inloggen(ActionEvent event) throws APIException, Auth0Exception {
+		try {
+			dc.meldAan(gebruikersnaam.getText(), wachtwoord.getText());
+			SideBarController np = new SideBarController(dc);
+			Scene scene = new Scene (np);
+			Stage stage = (Stage) this.getScene().getWindow();
+			stage.setScene(scene);
+			stage.show();
+		}catch(Auth0Exception a) {
+			wrongLogIn.setText("Foute gebruikersnaam en/of wachtwoord");
+		}
 	}
 
 }
