3d40584612e68f1131bc58616b738bc255cff162
Viktor Van Puyvelde <viktor.vanpuyvelde2@student.hogent.be>
2022-05-10 17:11:25 +0200

    change of wrong import

--- src/gui/NieuweDatasourcePaneelController.java
+++ src/gui/NieuweDatasourcePaneelController.java
@@ -1,7 +1,5 @@
 package gui;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,102 +14,106 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-public class NieuweDatasourcePaneelController extends GridPane {
+public class NieuweDatasourcePaneelController extends GridPane
+{
 
 	private DomeinController dc;
 	private MvoController mc;
-	
+
 	@FXML
-    private TextField naam_textfield;
+	private TextField naam_textfield;
 
 	@FXML
 	private Button upload_btn;
-	
+
 	@FXML
-    private Label fileLbl;
-	
+	private Label fileLbl;
+
 	@FXML
 	private ListView<String> mvosList;
-	
-    @FXML
-    private Button toevoegen_btn;
-    
-    @FXML
-    private Label toevoegenLbl;
-    
-    private ObservableList<Mvo> mvoList;
-    
-    public NieuweDatasourcePaneelController(DomeinController dc) {
+
+	@FXML
+	private Button toevoegen_btn;
+
+	@FXML
+	private Label toevoegenLbl;
+
+	private ObservableList<Mvo> mvoList;
+
+	public NieuweDatasourcePaneelController(DomeinController dc)
+	{
 		this.dc = dc;
 		this.mc = new MvoController();
 		buildGui();
 		setMvoList();
 
 	}
-    
-    private void buildGui() {
-		try{
-						
+
+	private void buildGui()
+	{
+		try
+		{
+
 			mvosList = new ListView<>();
-	    	mvosList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-			
+			mvosList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
 			mvoList = FXCollections.observableArrayList(new ArrayList<Mvo>());
-			
+
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("NieuweDatasourcePaneel.fxml"));
 			loader.setController(this);
 			loader.setRoot(this);
 			loader.load();
-			
-			
-			
-			} 
-		catch(IOException ex)
+
+		} catch (IOException ex)
 		{
-		throw new RuntimeException(ex);
+			throw new RuntimeException(ex);
 		}
 	}
-    
-    private void setMvoList() {
-    	List<Mvo> mvos = mc.geefMvos();
-    	for (Mvo m : mvos) {
-    		mvoList.add(m);
-    	}
-    	
-    	mvoList.forEach(m -> mvosList.getItems().add(m.getName()));    		
-    }
-    
-    @FXML
-    public void upload_OnAction(ActionEvent actionEvent) {
-    	FileChooser fileChooser = new FileChooser();
-    	fileChooser.setTitle("Open Csv File");
-    	fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Csv Files", "*.csv"));
-    	
-    	Stage newWindow = new Stage();
-    	newWindow.setTitle("Kies een csv bestand");
-    	File selectedFile = fileChooser.showOpenDialog(newWindow);
-    	
-    	if (selectedFile != null) {
-    		fileLbl.setText(String.format("%s geselecteerd", selectedFile.getName()));
-    		fileLbl.setTextFill(Color.GREEN);
-    		fileLbl.setStyle("-fx-font-weight: bold");
-    	}
-    }
-    
-    @FXML
-    public void toevoegen_OnAction(ActionEvent actionEvent) {
-    	toevoegenLbl.setText("Datasource toegevoegd!");
-    	toevoegenLbl.setTextFill(Color.GREEN);
-    	toevoegenLbl.setStyle("-fx-font-weight: bold");
-    }
+
+	private void setMvoList()
+	{
+		List<Mvo> mvos = mc.geefMvos();
+		for (Mvo m : mvos)
+		{
+			mvoList.add(m);
+		}
+
+		mvoList.forEach(m -> mvosList.getItems().add(m.getName()));
+	}
+
+	@FXML
+	public void upload_OnAction(ActionEvent actionEvent)
+	{
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open Csv File");
+		fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Csv Files", "*.csv"));
+
+		Stage newWindow = new Stage();
+		newWindow.setTitle("Kies een csv bestand");
+		File selectedFile = fileChooser.showOpenDialog(newWindow);
+
+		if (selectedFile != null)
+		{
+			fileLbl.setText(String.format("%s geselecteerd", selectedFile.getName()));
+			fileLbl.setTextFill(Color.GREEN);
+			fileLbl.setStyle("-fx-font-weight: bold");
+		}
+	}
+
+	@FXML
+	public void toevoegen_OnAction(ActionEvent actionEvent)
+	{
+		toevoegenLbl.setText("Datasource toegevoegd!");
+		toevoegenLbl.setTextFill(Color.GREEN);
+		toevoegenLbl.setStyle("-fx-font-weight: bold");
+	}
 }
--- src/module-info.java
+++ src/module-info.java
@@ -15,12 +15,12 @@ open module java_gr14
 	requires javafx.graphics;
 
 	// Unit tests
-	//requires org.junit.jupiter.api;
-	//requires org.junit.jupiter.params;
+	// requires org.junit.jupiter.api;
+	// requires org.junit.jupiter.params;
 
 	// Other
 	requires java.desktop;
 	requires auth0;
 	requires java.dotenv;
-	//requires org.junit.jupiter.api;
+	requires org.junit.jupiter.api;
 }
