47e4db7d234e843ace8808a3038ac4106ab1d4c1
Viktor Van Puyvelde <viktor.vanpuyvelde2@student.hogent.be>
2022-05-16 11:55:27 +0200

    categoriebuilder finished

--- src/domein/CategorieController.java
+++ src/domein/CategorieController.java
@@ -3,25 +3,26 @@ package domein;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import exceptions.InformationRequiredException;
 import repository.CategorieDao;
 import repository.CategorieDaoJpa;
 
 public class CategorieController
 {
 	private CategorieDao categorieRepo;
-	
+
 	public CategorieController()
 	{
 		setCategorieRepo(new CategorieDaoJpa());
-		//populateDB();
+		// populateDB();
 	}
-	
+
 	public List<Categorie> geefCategorien()
 	{
 		return categorieRepo.findAll();
 
 	}
-	
+
 	public void setCategorieRepo(CategorieDao categorieRepo)
 	{
 		this.categorieRepo = categorieRepo;
@@ -30,29 +31,48 @@ public class CategorieController
 	public List<String> geefAlleEchteCategorienNaam()
 	{
 		List<Categorie> catList = categorieRepo.geefAlleEchteCategorieen();
-		
+
 		return catList.stream().map(Categorie::getName).collect(Collectors.toList());
 
 	}
 
-	public void voegCategorieToe(String name, String iconName, List<String> roles)
+	public void voegCategorieToe(String name, String iconName, List<String> roles, List<Sdg> sdgs)
+			throws InformationRequiredException
 	{
+		Categorie newCategorie = createCategorie(null, name, iconName, roles, sdgs);
 		CategorieDaoJpa.startTransaction();
-		categorieRepo.insert(new Categorie(name, iconName, roles, true));
+		categorieRepo.insert(new Categorie(name, iconName, roles, true, sdgs));
 		CategorieDaoJpa.commitTransaction();
 	}
-	
+
+	private Categorie createCategorie(CategorieBuilder cb, String name, String iconName, List<String> roles,
+			List<Sdg> sdgs) throws InformationRequiredException
+	{
+		if (cb == null)
+		{
+			cb = new CategorieBuilder();
+		}
+		cb.createNewCategorie();
+		cb.buildId();
+		cb.buildName(name);
+		cb.buildIconName(iconName);
+		cb.buildSdgs(sdgs);
+		cb.buildRoles(roles);
+		return cb.getCategorie();
+	}
+
 	public void close()
 	{
 		CategorieDaoJpa.closePersistency();
 	}
 
-	public List<Sdg> geefSdgsVoorCategorie(Categorie c) {
+	public List<Sdg> geefSdgsVoorCategorie(Categorie c)
+	{
 		List<Sdg> sdgs = categorieRepo.geefSdgVoorCategorie(c.getName());
 		c.setSdgs(sdgs);
 		return sdgs;
 	}
-	
+
 //	private void populateDB() {
 //		categorieRepo.insert(new Categorie("Profit", "icon1", new String[] {"manager", "stakeholder", "coördinator"}, true));
 //		categorieRepo.insert(new Categorie("Planet", "icon2", new String[] {"manager", "stakeholder", "coördinator"}, true));
