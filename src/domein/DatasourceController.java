6ff3cc347f705dc65cf7c2ef7c3eaa6e10e78b7c
Niels Van Hove <niels.vanhove@student.hogent.be>
2022-05-19 17:04:10 +0200

    datacontroller refactor

--- src/domein/DatasourceController.java
+++ src/domein/DatasourceController.java
@@ -1,5 +1,6 @@
 package domein;
 
+import java.util.Collections;
 import java.util.List;
 
 import repository.DatasourceDao;
@@ -8,15 +9,17 @@ import repository.DatasourceDaoJpa;
 public class DatasourceController
 {
 	private DatasourceDao datasourceRepo;
-
+	private List<Datasource> datasources;
+	
 	public DatasourceController()
 	{
 		setDatasourceRepo(new DatasourceDaoJpa());
+		datasources = datasourceRepo.findAll();
 	}
 
 	public List<Datasource> geefDatasources()
 	{
-		return datasourceRepo.findAll();
+		return Collections.unmodifiableList(datasources);
 	}
 
 	public Datasource geefDatasourceDoorNaam(String naam)
@@ -35,17 +38,21 @@ public class DatasourceController
 	}
 	
 	public void voegDatasourceToe(String naam, boolean flag) {
+		Datasource d = new Datasource(naam, flag);
 		DatasourceDaoJpa.startTransaction();
-		this.datasourceRepo.insert(new Datasource(naam, flag));
+		this.datasourceRepo.insert(d);
 		DatasourceDaoJpa.commitTransaction();
+		datasources.add(d);
 	}	
 	
 	public void updateDatasource(Datasource d, String naam, boolean flag) {
+		int index = datasources.indexOf(d);
 		d.setName(naam);
 		d.setFlag(flag);
 		DatasourceDaoJpa.startTransaction();
 		this.datasourceRepo.update(d);
 		DatasourceDaoJpa.commitTransaction();
+		datasources.set(index, d);
 	}
 
 	public void deleteDatasource(Datasource d) {
