9724775db8cec3b167538dbdfdf1656db7bcb597
Niels Van Hove <niels.vanhove@student.hogent.be>
2022-05-22 10:52:19 +0200

    Usertest met .env

--- src/test/TestUser.java
+++ src/test/TestUser.java
@@ -5,15 +5,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import com.auth0.exception.APIException;
 import com.auth0.exception.Auth0Exception;
 
 import domein.UserController;
 import exceptions.IngelogdVerkeerdeRol;
+import io.github.cdimascio.dotenv.Dotenv;
 
 class TestUser {
 
@@ -26,14 +24,18 @@ class TestUser {
 	
 	@Test
 	public void testAanmeldenGoed() throws APIException, Auth0Exception {
-		controller.meldAan("admin@test.be", "Azertyuiop.123");
+		Dotenv dotenv = Dotenv.load();
+
+		controller.meldAan(dotenv.get("USERTEST_GOED"), dotenv.get("USERTEST_GOED_WW"));
 		
 		Assertions.assertEquals("admin", controller.getUserInfo());
 	}
 
 	@Test
 	public void testAanmeldenVerkeerdeRol() {
-		Assertions.assertThrows(IngelogdVerkeerdeRol.class, () -> controller.meldAan("test@test.be", "Azertyuiop.123"));
+		Dotenv dotenv = Dotenv.load();
+
+		Assertions.assertThrows(IngelogdVerkeerdeRol.class, () -> controller.meldAan(dotenv.get("USERTEST_FOUT"), dotenv.get("USERTEST_FOUT_WW")));
 	}
 	
 	@ParameterizedTest
@@ -41,4 +43,15 @@ class TestUser {
 	public void testAanmeldenVerkeerdeNaamOfWW(String email, String ww) throws APIException, Auth0Exception {
 		Assertions.assertThrows(Auth0Exception.class, () -> controller.meldAan(email, ww));
 	}	
+	
+	@Test
+	public void testAfmelden() throws APIException, Auth0Exception {
+		Dotenv dotenv = Dotenv.load();
+
+		controller.meldAan(dotenv.get("USERTEST_GOED"), dotenv.get("USERTEST_GOED_WW"));
+		controller.meldAf();
+		
+		Assertions.assertEquals(null, controller.getCoordinator());
+	}
+
 }
